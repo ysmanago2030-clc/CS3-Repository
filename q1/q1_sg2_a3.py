b721d512e234f926b34e78e8308436610cde185f
ysmanago2030-clc <ysmanago2030@clc.pshs.edu.ph>
2026-08-15 17:10:53 +0800

    Update q1_sg2_a3.py

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,3 +1,4 @@
+#list containing all zodiac signs per year, assigned to a certain index
 zodiac_sign = [
     "Rat (鼠 / Shǔ)",
     "Ox (牛 / Niú)",
@@ -13,11 +14,11 @@ zodiac_sign = [
     "Pig (猪 / Zhū)"
 ]
 
+#main input statement where it asks the integer value of user's birth year
 user_year = int(input("Enter your birth year: "))
 
+#calculates zodiac year and checks for valid inputs (>= 1900)
 if user_year >= 1900:
     print("Your zodiac sign is :", zodiac_sign[(user_year - 1900) % 12])
-    
 else:
     print("Invalid Year, it should not be earlier than 1900")
-    
