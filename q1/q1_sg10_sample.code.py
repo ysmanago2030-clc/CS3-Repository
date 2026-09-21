2884895552a78bd9f64fe935f563d955c7ac84ab
ysmanago2030-clc <ysmanago2030@clc.pshs.edu.ph>
2026-09-21 22:09:48 +0800

    Add files via upload

---
+++ q1/q1_sg10_sample.code.py
@@ -0,0 +1,21 @@
+class Tusoktusok:
+    name = ""
+    sauce = None
+    def __init__(self,name):
+        self.name = name
+    def dip(self,sauce):
+        self.sauce = sauce
+    def eat(self):
+        print("I ate",self.name,"with",self.sauce.name,"and it tastes",self.sauce.taste)
+
+class Sauce:
+    name = ""
+    taste = ""
+    def __init__(self,name,taste):
+        self.name = name
+        self.taste = taste
+
+fishball = Tusoktusok("fishball")
+vinegar = Sauce("vinegar","sour")
+fishball.dip(vinegar)
+fishball.eat()
